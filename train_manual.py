47d5f439f843645b35818694ecae02d44a6d48fa
mcalibk <bkieranmcauliffe@gmail.com>
2021-09-14 16:09:07 -0400

    model saving

--- train_manual.py
+++ train_manual.py
@@ -120,108 +120,116 @@ def preprocess_dataset(files: list):
         get_spectrogram_and_label_id, num_parallel_calls=AUTOTUNE)
     return output_ds
 
-train_files = filenames[:4000]
-val_files = filenames[4000:4400]
-test_files = filenames[4400:]
-files_ds = tf.data.Dataset.from_tensor_slices(train_files)
-waveform_ds = files_ds.map(get_waveform_and_label, num_parallel_calls=AUTOTUNE)
-plot_waveforms(waveform_ds)
-spectrogram_ds = waveform_ds.map(get_spectrogram_and_label_id, num_parallel_calls=AUTOTUNE)
+if __name__ == "__main__":
+    total_files = len(filenames)
+    print(f"total files = {total_files}")
+    train_count = (len(filenames) // 5) * 4
+    val_count = total_files // 10
+    print(f"Training with {train_count} training files, {val_count} validation files, and {len(filenames) - val_count - train_count} test files")
+    train_files = filenames[:train_count]
+    val_files = filenames[train_count:train_count + val_count]
+    test_files = filenames[train_count + val_count:]
+    files_ds = tf.data.Dataset.from_tensor_slices(train_files)
+    waveform_ds = files_ds.map(get_waveform_and_label, num_parallel_calls=AUTOTUNE)
+    plot_waveforms(waveform_ds)
+    spectrogram_ds = waveform_ds.map(get_spectrogram_and_label_id, num_parallel_calls=AUTOTUNE)
 
 
 
-train_ds = spectrogram_ds
-val_ds = preprocess_dataset(val_files)
-test_ds = preprocess_dataset(test_files)
-plot_spectrograms(train_ds)
-
-
-
-
-
-batch_size = 64
-train_ds = train_ds.batch(batch_size)
-val_ds = val_ds.batch(batch_size)
-train_ds = train_ds.cache().prefetch(AUTOTUNE)
-val_ds = val_ds.cache().prefetch(AUTOTUNE)
+    train_ds = spectrogram_ds
+    val_ds = preprocess_dataset(val_files)
+    test_ds = preprocess_dataset(test_files)
+    plot_spectrograms(train_ds)
 
 
 
 
 
-
-sampleset = iter(spectrogram_ds)
-for i in range(1):
-    spectrogram, _ = next(sampleset)
-    successful = True
-    input_shape = spectrogram.shape
-
-print('Input shape:', input_shape)
-num_labels = len(techniques)
-
-norm_layer = preprocessing.Normalization()
-just_features = spectrogram_ds.map(lambda x, _: x)
-
-norm_layer.adapt(just_features)
-
-
-model = models.Sequential([
-    layers.Input(shape=input_shape),
-    preprocessing.Resizing(32, 32),
-    norm_layer,
-    layers.Conv2D(32, 3, activation='relu'),
-    layers.Conv2D(64, 3, activation='relu'),
-    layers.MaxPooling2D(),
-    layers.Dropout(0.25),
-    layers.Flatten(),
-    layers.Dense(128, activation='relu'),
-    layers.Dropout(0.5),
-    layers.Dense(num_labels),
-])
-
-model.summary()
-
-model.compile(
-    optimizer=tf.keras.optimizers.Adam(),
-    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-    metrics=['accuracy'],
-)
+    batch_size = 64
+    train_ds = train_ds.batch(batch_size)
+    val_ds = val_ds.batch(batch_size)
+    train_ds = train_ds.cache().prefetch(AUTOTUNE)
+    val_ds = val_ds.cache().prefetch(AUTOTUNE)
 
 
 
-EPOCHS = 10
-history = model.fit(
-    train_ds,
-    validation_data=val_ds,
-    epochs=EPOCHS,
-    callbacks=tf.keras.callbacks.EarlyStopping(verbose=1, patience=2),
-)
 
-metrics = history.history
-plt.plot(history.epoch, metrics['loss'], metrics['val_loss'])
-plt.legend(['loss', 'val_loss'])
-plt.show()
 
-test_audio = []
-test_labels = []
 
-for audio, label in test_ds:
-  test_audio.append(audio.numpy())
-  test_labels.append(label.numpy())
+    sampleset = iter(spectrogram_ds)
+    for i in range(1):
+        spectrogram, _ = next(sampleset)
+        successful = True
+        input_shape = spectrogram.shape
 
-test_audio = np.array(test_audio)
-test_labels = np.array(test_labels)
+    print('Input shape:', input_shape)
+    num_labels = len(techniques)
 
-y_pred = np.argmax(model.predict(test_audio), axis=1)
-y_true = test_labels
+    norm_layer = preprocessing.Normalization()
+    just_features = spectrogram_ds.map(lambda x, _: x)
 
-test_acc = sum(y_pred == y_true) / len(y_true)
-print(f'Test set accuracy: {test_acc:.0%}')
+    norm_layer.adapt(just_features)
 
-confusion_mtx = tf.math.confusion_matrix(y_true, y_pred)
-plt.figure(figsize=(10, 8))
-sns.heatmap(confusion_mtx, xticklabels=techniques, yticklabels=techniques,
-            annot=True, fmt='g')
-plt.xlabel('Prediction')
-plt.ylabel('Label')
-plt.show()
+
+    model = models.Sequential([
+        layers.Input(shape=input_shape),
+        preprocessing.Resizing(32, 32),
+        norm_layer,
+        layers.Conv2D(32, 3, activation='relu'),
+        layers.Conv2D(64, 3, activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Dropout(0.25),
+        layers.Flatten(),
+        layers.Dense(128, activation='relu'),
+        layers.Dropout(0.5),
+        layers.Dense(num_labels),
+    ])
+
+    model.summary()
+
+    model.compile(
+        optimizer=tf.keras.optimizers.Adam(),
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        metrics=['accuracy'],
+    )
+
+
+
+    EPOCHS = 10
+    history = model.fit(
+        train_ds,
+        validation_data=val_ds,
+        epochs=EPOCHS,
+        callbacks=tf.keras.callbacks.EarlyStopping(verbose=1, patience=2),
+    )
+
+    metrics = history.history
+    plt.plot(history.epoch, metrics['loss'], metrics['val_loss'])
+    plt.legend(['loss', 'val_loss'])
+    plt.show()
+
+    test_audio = []
+    test_labels = []
+
+    for audio, label in test_ds:
+      test_audio.append(audio.numpy())
+      test_labels.append(label.numpy())
+
+    test_audio = np.array(test_audio)
+    test_labels = np.array(test_labels)
+
+    y_pred = np.argmax(model.predict(test_audio), axis=1)
+    y_true = test_labels
+
+    test_acc = sum(y_pred == y_true) / len(y_true)
+    print(f'Test set accuracy: {test_acc:.0%}')
+
+    confusion_mtx = tf.math.confusion_matrix(y_true, y_pred)
+    plt.figure(figsize=(10, 8))
+    sns.heatmap(confusion_mtx, xticklabels=techniques, yticklabels=techniques,
+                annot=True, fmt='g')
+    plt.xlabel('Prediction')
+    plt.ylabel('Label')
+    plt.show()
+
+    model.save('savedModel')
