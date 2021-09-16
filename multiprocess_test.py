bc6f9ad35f3033155ed517ed05172b85b050666c
mcalibk <bkieranmcauliffe@gmail.com>
2021-09-16 13:05:20 -0400

    all sub processes wait to start together

--- multiprocess_test.py
+++ multiprocess_test.py
@@ -3,9 +3,12 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 import time
-from multiprocessing import Lock, Process, Queue, current_process
+from multiprocessing import Lock, Process, Queue, current_process, Value
 import queue
 import librosa
+
+####### FIX THIS SO THAT TECHNIQUES ARE PROPERLY ORDERED $$$$$
+
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # use GPU instead of AVX
 
@@ -46,18 +49,23 @@ def parse_result(prediction, tf):
     return prediction_ranks
 
 
-def identification_process(unidentified_notes: Queue, identified_notes: Queue):
+def identification_process(unidentified_notes: Queue, identified_notes: Queue,
+                           ready_count: Value, finished: Value, ready: Value):
     import tensorflow as tfp
     techniques = []
     print(f"Starting process {current_process().name}")
     model = tfp.keras.models.load_model("savedModel")
     print(f"Model loaded for {current_process().name}")
+    ready_count.value += 1
+    while ready.value == 0:  # wait for all processes to be ready
+        pass
     while True:
+        if finished.value == 1:  # the main process says it's time to quit
+            break
         try:
             note = unidentified_notes.get_nowait()
         except queue.Empty:
-            print(f"Queue empty, {current_process().name} exiting")
-            break
+            pass
         else:
 
             ds = prep_data(note, tfp)
@@ -71,34 +79,47 @@ def identification_process(unidentified_notes: Queue, identified_notes: Queue):
 
 
 def main():
+    techniques = ["_", "smack", "idk", "tasto"]
     number_of_processes = 3
+
+    unidentified_notes = Queue()  # stores waveforms of prepared notes that must be identified
+    identified_notes = Queue()  # stores arrays of ints indicating the most to least likely technique
+    ready_count = Value('i', 0) #  track how many processes are ready
+    finished = Value('i', 0)  # track how many processes are finished
+    ready = Value('i', 0)
+
     processes = []
 
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-
-    sample_files = [f"samples/manual/smack/smack1{i}.wav" for i in range(2, 9)]
-    unidentified_notes = Queue()
-    identified_notes = Queue()
+    sample_files = [f"samples/manual/chord/chord{i}.wav" for i in range(10, 196)]
 
     for f in sample_files:
-        print(f)
         unidentified_notes.put(librosa.load(f)[0])
 
     for w in range(number_of_processes):
-        p = Process(target=identification_process, args=(unidentified_notes, identified_notes))
+        p = Process(target=identification_process,
+                    args=(unidentified_notes, identified_notes, ready_count, finished, ready))
         processes.append(p)
         p.start()
-    cur_time = time.time()
-    for p in processes:
-        p.join()
-    print("All processes finished")
-    finished_notes = []
-    while not identified_notes.empty():
-        finished_notes.append(identified_notes.get())
-    print(f"Total time : {time.time() - cur_time}")
 
-    for n in finished_notes:
-        print(n)
+    while ready_count.value != number_of_processes:
+        pass
+    cur_time = time.time()
+    print("All processes ready")
+    ready.value = 1
+
+    identified_notes_count = 0
+    ready_to_quit = False
+    while True:
+        if identified_notes_count == len(sample_files):
+            ready_to_quit = True
+        if not identified_notes.empty():
+            print(f"New identified note in main process: {identified_notes.get()}")
+            identified_notes_count += 1
+        if ready_to_quit:
+            finished.value = 1
+            break
+
+    print(f"Total time : {time.time() - cur_time}, notes identified = {identified_notes_count}")
 
 if __name__ == "__main__":
     main()
