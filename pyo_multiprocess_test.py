af2d3fb4578cfced9ccd8ec52396812cea320889
mcalibk <bkieranmcauliffe@gmail.com>
2021-09-17 12:15:04 -0400

    guitar input configured

--- pyo_multiprocess_test.py
+++ pyo_multiprocess_test.py
@@ -1,13 +1,11 @@
-
 import os
-import matplotlib.pyplot as plt
 import numpy as np
 import time
-from multiprocessing import Lock, Process, Queue, current_process, Value
+from multiprocessing import Process, Queue, current_process, Value
 import queue
 import librosa
 from utilities import find_onsets
-
+from pyo import *
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # use GPU instead of AVX
 
@@ -19,18 +17,17 @@ def get_waveform(audio, tf):
     return audio
 
 
-
 def get_spectrogram(waveform, tf):
-      if tf.shape(waveform) > 16000:
+    if tf.shape(waveform) > 16000:
         waveform = waveform[:16000]
-      zero_padding = tf.zeros([16000] - tf.shape(waveform), dtype=tf.float32)  # fix this so the padding isn't huge
+    zero_padding = tf.zeros([16000] - tf.shape(waveform), dtype=tf.float32)  # fix this so the padding isn't huge
 
-      waveform = tf.cast(waveform, tf.float32)
-      equal_length = tf.concat([waveform, zero_padding], 0)
-      spectrogram = tf.signal.stft(
+    waveform = tf.cast(waveform, tf.float32)
+    equal_length = tf.concat([waveform, zero_padding], 0)
+    spectrogram = tf.signal.stft(
         equal_length, frame_length=255, frame_step=128)
 
-      return spectrogram
+    return spectrogram
 
 
 def prep_data(note: np.ndarray, tf):
@@ -39,6 +36,7 @@ def prep_data(note: np.ndarray, tf):
     spec = get_spectrogram(waveform, tf)
     ds = tf.data.Dataset.from_tensors([spec])
     return ds
+
 
 def parse_result(prediction, tf):
     sftmax = tf.nn.softmax(prediction[0])
@@ -88,19 +86,21 @@ def note_split_process(buffer_excerpts: Queue, unidentified_notes: Queue, finish
         except queue.Empty:
             pass
         else:
-            print(buf_excerpt)
+            print("New buffer identified to split")
             onsets = find_onsets(buf_excerpt, 22050)
+            if len(onsets) == 0:
+                print("buffer contains no onsets")
+                continue
             first_onset = onsets[0]
-            first_note = np.concatenate([leftover_buf, buf_excerpt[:first_onset]])  #use the leftover from the last buffer
+            first_note = np.concatenate(
+                [leftover_buf, buf_excerpt[:first_onset]])  # use the leftover from the last buffer
             unidentified_notes.put(first_note)
-            for i, onset in enumerate(onsets[1:-2]):  #Don't do this for the first or last onset
+            for i, onset in enumerate(onsets[1:-2]):  # Don't do this for the first or last onset
                 start = onset
                 finish = onsets[i + 2]  # +2 because of the funny indexing
                 new_note = buf_excerpt[start:finish]
                 unidentified_notes.put(new_note)
     return True
-
-
 
 
 def main():
@@ -110,6 +110,8 @@ def main():
     buffer_length = 2  # value in seconds
     sr = 22050
 
+
+    ###### Set up shared information ######
     buffer_excerpts = Queue()  # contains 2 second snippets of buffer that needs to be split into notes
     unidentified_notes = Queue()  # stores waveforms of prepared notes that must be identified
     identified_notes = Queue()  # stores arrays of ints indicating the most to least likely technique
@@ -117,26 +119,34 @@ def main():
     finished = Value('i', 0)  # track how many processes are finished
     ready = Value('i', 0)  # signal to all subprocesses that we can start
 
-    processes = []
 
-    sample_file = f"samples/sorted/chords#02.wav"
+    ###### Set up PYO #######
+    s = Server()
+    s.setInputDevice(9)
+    s.boot()
+    t = NewTable(length=buffer_length)
+    inp = Input()
+    rec = TableRec(inp, table=t).play()
 
-    ###### Split the sample into 2 second chunks ######
-    print("Splitting huge audio file...")
-    sample_wav, _ = librosa.load(sample_file, sr)
-    total_buffers = (len(sample_wav) // sr) + 1
-    print(total_buffers)
-    for i in range(total_buffers):
-        buf_excerpt = sample_wav[i * sr * buffer_length: (i + 1) * sr * buffer_length]
-        buffer_excerpts.put(buf_excerpt)
+    def send_buf_for_analysis():
+        print("Sending out new buffer")
+        np_arr = np.frombuffer(t.getBuffer())
+        buffer_excerpts.put(np_arr)
+        rec.play()
+
+    tf = TrigFunc(rec["trig"], send_buf_for_analysis)
+
 
     ###### Start all the processes ######
+    print("Loading tensorflow models...")
+    processes = []
     for w in range(number_of_processes):
         p = Process(target=identification_process,
                     args=(unidentified_notes, identified_notes, ready_count, finished, ready))
         processes.append(p)
         p.start()
 
+    print("Starting note split...")
     note_split = Process(target=note_split_process, args=(buffer_excerpts, unidentified_notes, finished))
     note_split.start()
 
@@ -146,16 +156,14 @@ def main():
     print("All processes ready")
     ready.value = 1
 
-
-
     identified_notes_count = 0
     ready_to_quit = False
 
-    while True:
-        if identified_notes_count == total_buffers:
-            ready_to_quit = True
-        if not identified_notes.empty():
+    ###### Finally run the PYO server ######
+    s.start()
 
+    while True:
+        if not identified_notes.empty():
             str_results = list(map(lambda i: techniques[i], identified_notes.get()))
             str_results = str_results[0:-1]
             str_results.reverse()
@@ -166,6 +174,7 @@ def main():
             break
 
     print(f"Total time : {time.time() - cur_time}, notes identified = {identified_notes_count}")
+
 
 if __name__ == "__main__":
     main()
