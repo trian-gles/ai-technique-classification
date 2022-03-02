87cb64d304f72de6bd569fe075cdd766faf7dd67
mcalibk <bkieranmcauliffe@gmail.com>
2022-03-02 10:40:08 -0500

    send_clar

--- webrtcmix/max_query.py
+++ webrtcmix/max_query.py
@@ -7,7 +7,11 @@ class Client:
     def send_arps(self, f1: float, f2: float, f3: float):
         self._client.send_message("/arps", [f1, f2, f3])
 
+    def send_clar(self):
+        self._client.send_message("/clar", 0.0)
+
 
 if __name__ == "__main__":
     c = Client("127.0.0.1")
     c.send_arps(440, 880, 1220)
+    c.send_clar()
